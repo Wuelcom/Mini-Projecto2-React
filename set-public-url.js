2956ebc8001e915a01e17427405df196b30b9aee
Allan Caro <Wuelcom@users.noreply.github.com.>
2025-11-06 10:45:31 -0500

    Trabajo hecho

--- set-public-url.js
+++ set-public-url.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const isVercel = process.env.VERCEL === "1";
 const homepage = isVercel
   ? "" // En Vercel, el sitio vive en el dominio raíz
-  : "https://brandondiaz18.github.io/Proyecto_Jueves/"; // En GitHub Pages, usamos la subcarpeta
+  : "https://wuelcom.github.io/Mini-Projecto2-React/"; // En GitHub Pages, usamos la subcarpeta
 
 const envContent = `PUBLIC_URL=${homepage}\n`;
 fs.writeFileSync("./.env.production", envContent);
